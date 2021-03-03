dfa9f58d7f2f2bdde69b0c37bc1b28c0f24911b2
Qistina-Everleagues <qistina.mahmud@everleagues.com>
2021-03-03 20:39:46 +0800

    Update 3/2

--- src/app/eggdonation/eggdonation.component.ts
+++ src/app/eggdonation/eggdonation.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import { HttpClient, HttpEventType } from "@angular/common/http";
+import { FormBuilder, FormGroup, Validators, FormArray} from '@angular/forms';
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-eggdonation',
   templateUrl: './eggdonation.component.html',
-  styleUrls: ['./eggdonation.component.css']
+  styleUrls: ['./eggdonation.component.css'],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: {displayDefaultIndicatorType: false}
+  }]
 })
 export class EggdonationComponent implements OnInit {
   checked = false;
-  isLinear = false;
+  isLinear = true;
+  TotalRow: number;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
   forthFormGroup: FormGroup;
   fifthFormGroup: FormGroup;
   sixthFormGroup: FormGroup;
+  siblingsFormGroup: FormGroup;
 
   constructor(private _formBuilder: FormBuilder, private http: HttpClient) { }
 
@@ -27,118 +33,222 @@ export class EggdonationComponent implements OnInit {
       self_ethnic: ['', Validators.required],
       self_genetic: ['', Validators.required],
       self_history: ['', Validators.required],
-      self_medic: ['', Validators.required],
+      self_medic_N_A: [false, Validators.required],
+      self_medic_Blood: [false, Validators.required],
+      self_medic_Heart: [false, Validators.required],
+      self_medic_Respiratory: [false, Validators.required],
+      self_medic_Gastrointestinal: [false, Validators.required],
+      self_medic_Metabolic: [false, Validators.required],
+      self_medic_Urinary: [false, Validators.required],
+      self_medic_Reproductive: [false, Validators.required],
+      self_medic_Neurological: [false, Validators.required],
+      self_medic_Mental_Health: [false, Validators.required],
+      self_medic_Muscle: [false, Validators.required],
+      self_medic_Skin_Condition: [false, Validators.required],
+      self_medic_Others: [false, Validators.required],
       father_age: ['', Validators.required],
       father_living: ['', Validators.required],
       father_cause: ['', Validators.required],
       father_ethnic: ['', Validators.required],
       father_genetic: ['', Validators.required],
       father_history: ['', Validators.required],
-      father_medic: ['', Validators.required],
+      father_medic_N_A: [false, Validators.required],
+      father_medic_Blood: [false, Validators.required],
+      father_medic_Heart: [false, Validators.required],
+      father_medic_Respiratory: [false, Validators.required],
+      father_medic_Gastrointestinal: [false, Validators.required],
+      father_medic_Metabolic: [false, Validators.required],
+      father_medic_Urinary: [false, Validators.required],
+      father_medic_Reproductive: [false, Validators.required],
+      father_medic_Neurological: [false, Validators.required],
+      father_medic_Mental_Health: [false, Validators.required],
+      father_medic_Muscle: [false, Validators.required],
+      father_medic_Skin_Condition: [false, Validators.required],
+      father_medic_Others: [false, Validators.required],
       mother_age: ['', Validators.required],
       mother_living: ['', Validators.required],
       mother_cause: ['', Validators.required],
       mother_ethnic: ['', Validators.required],
       mother_genetic: ['', Validators.required],
       mother_history: ['', Validators.required],
-      mother_medic: ['', Validators.required],
+      mother_medic_N_A: [false, Validators.required],
+      mother_medic_Blood: [false, Validators.required],
+      mother_medic_Heart: [false, Validators.required],
+      mother_medic_Respiratory: [false, Validators.required],
+      mother_medic_Gastrointestinal: [false, Validators.required],
+      mother_medic_Metabolic: [false, Validators.required],
+      mother_medic_Urinary: [false, Validators.required],
+      mother_medic_Reproductive: [false, Validators.required],
+      mother_medic_Neurological: [false, Validators.required],
+      mother_medic_Mental_Health: [false, Validators.required],
+      mother_medic_Muscle: [false, Validators.required],
+      mother_medic_Skin_Condition: [false, Validators.required],
+      mother_medic_Others: [false, Validators.required],
       maternal_grandfather_age: ['', Validators.required],
       maternal_grandfather_living: ['', Validators.required],
       maternal_grandfather_cause: ['', Validators.required],
       maternal_grandfather_ethnic: ['', Validators.required],
       maternal_grandfather_genetic: ['', Validators.required],
       maternal_grandfather_history: ['', Validators.required],
-      maternal_grandfather_medic: ['', Validators.required],
+      maternal_grandfather_medic_N_A: [false, Validators.required],
+      maternal_grandfather_medic_Blood: [false, Validators.required],
+      maternal_grandfather_medic_Heart: [false, Validators.required],
+      maternal_grandfather_medic_Respiratory: [false, Validators.required],
+      maternal_grandfather_medic_Gastrointestinal: [false, Validators.required],
+      maternal_grandfather_medic_Metabolic: [false, Validators.required],
+      maternal_grandfather_medic_Urinary: [false, Validators.required],
+      maternal_grandfather_medic_Reproductive: [false, Validators.required],
+      maternal_grandfather_medic_Neurological: [false, Validators.required],
+      maternal_grandfather_medic_Mental_Health: [false, Validators.required],
+      maternal_grandfather_medic_Muscle: [false, Validators.required],
+      maternal_grandfather_medic_Skin_Condition: [false, Validators.required],
+      maternal_grandfather_medic_Others: [false, Validators.required],
       maternal_grandmother_age: ['', Validators.required],
       maternal_grandmother_living: ['', Validators.required],
       maternal_grandmother_cause: ['', Validators.required],
       maternal_grandmother_ethnic: ['', Validators.required],
       maternal_grandmother_genetic: ['', Validators.required],
       maternal_grandmother_history: ['', Validators.required],
-      maternal_grandmother_medic: ['', Validators.required],
+      maternal_grandmother_medic_N_A: [false, Validators.required],
+      maternal_grandmother_medic_Blood: [false, Validators.required],
+      maternal_grandmother_medic_Heart: [false, Validators.required],
+      maternal_grandmother_medic_Respiratory: [false, Validators.required],
+      maternal_grandmother_medic_Gastrointestinal: [false, Validators.required],
+      maternal_grandmother_medic_Metabolic: [false, Validators.required],
+      maternal_grandmother_medic_Urinary: [false, Validators.required],
+      maternal_grandmother_medic_Reproductive: [false, Validators.required],
+      maternal_grandmother_medic_Neurological: [false, Validators.required],
+      maternal_grandmother_medic_Mental_Health: [false, Validators.required],
+      maternal_grandmother_medic_Muscle: [false, Validators.required],
+      maternal_grandmother_medic_Skin_Condition: [false, Validators.required],
+      maternal_grandmother_medic_Others: [false, Validators.required],
       paternal_grandfather_age: ['', Validators.required],
       paternal_grandfather_living: ['', Validators.required],
       paternal_grandfather_cause: ['', Validators.required],
       paternal_grandfather_ethnic: ['', Validators.required],
       paternal_grandfather_genetic: ['', Validators.required],
       paternal_grandfather_history: ['', Validators.required],
-      paternal_grandfather_medic: ['', Validators.required],
+      paternal_grandfather_medic_N_A: [false, Validators.required],
+      paternal_grandfather_medic_Blood: [false, Validators.required],
+      paternal_grandfather_medic_Heart: [false, Validators.required],
+      paternal_grandfather_medic_Respiratory: [false, Validators.required],
+      paternal_grandfather_medic_Gastrointestinal: [false, Validators.required],
+      paternal_grandfather_medic_Metabolic: [false, Validators.required],
+      paternal_grandfather_medic_Urinary: [false, Validators.required],
+      paternal_grandfather_medic_Reproductive: [false, Validators.required],
+      paternal_grandfather_medic_Neurological: [false, Validators.required],
+      paternal_grandfather_medic_Mental_Health: [false, Validators.required],
+      paternal_grandfather_medic_Muscle: [false, Validators.required],
+      paternal_grandfather_medic_Skin_Condition: [false, Validators.required],
+      paternal_grandfather_medic_Others: [false, Validators.required],
       paternal_grandmother_age: ['', Validators.required],
       paternal_grandmother_living: ['', Validators.required],
       paternal_grandmother_cause: ['', Validators.required],
       paternal_grandmother_ethnic: ['', Validators.required],
       paternal_grandmother_genetic: ['', Validators.required],
       paternal_grandmother_history: ['', Validators.required],
-      paternal_grandmother_medic: ['', Validators.required],
+      paternal_grandmother_medic_N_A: [false, Validators.required],
+      paternal_grandmother_medic_Blood: [false, Validators.required],
+      paternal_grandmother_medic_Heart: [false, Validators.required],
+      paternal_grandmother_medic_Respiratory: [false, Validators.required],
+      paternal_grandmother_medic_Gastrointestinal: [false, Validators.required],
+      paternal_grandmother_medic_Metabolic: [false, Validators.required],
+      paternal_grandmother_medic_Urinary: [false, Validators.required],
+      paternal_grandmother_medic_Reproductive: [false, Validators.required],
+      paternal_grandmother_medic_Neurological: [false, Validators.required],
+      paternal_grandmother_medic_Mental_Health: [false, Validators.required],
+      paternal_grandmother_medic_Muscle: [false, Validators.required],
+      paternal_grandmother_medic_Skin_Condition: [false, Validators.required],
+      paternal_grandmother_medic_Others: [false, Validators.required],
       sibling1_age: ['', Validators.required],
       sibling1_living: ['', Validators.required],
       sibling1_cause: ['', Validators.required],
       sibling1_ethnic: ['', Validators.required],
       sibling1_genetic: ['', Validators.required],
       sibling1_history: ['', Validators.required],
-      sibling1_medic: ['', Validators.required],
+      sibling1_medic_N_A: [false, Validators.required],
+      sibling1_medic_Blood: [false, Validators.required],
+      sibling1_medic_Heart: [false, Validators.required],
+      sibling1_medic_Respiratory: [false, Validators.required],
+      sibling1_medic_Gastrointestinal: [false, Validators.required],
+      sibling1_medic_Metabolic: [false, Validators.required],
+      sibling1_medic_Urinary: [false, Validators.required],
+      sibling1_medic_Reproductive: [false, Validators.required],
+      sibling1_medic_Neurological: [false, Validators.required],
+      sibling1_medic_Mental_Health: [false, Validators.required],
+      sibling1_medic_Muscle: [false, Validators.required],
+      sibling1_medic_Skin_Condition: [false, Validators.required],
+      sibling1_medic_Others: [false, Validators.required],
       sibling2_age: ['', Validators.required],
       sibling2_living: ['', Validators.required],
       sibling2_cause: ['', Validators.required],
       sibling2_ethnic: ['', Validators.required],
       sibling2_genetic: ['', Validators.required],
       sibling2_history: ['', Validators.required],
-      sibling2_medic: ['', Validators.required],
-      more_sibling_age: ['', Validators.required],
-      more_sibling_living: ['', Validators.required],
-      more_sibling_cause: ['', Validators.required],
-      more_sibling_ethnic: ['', Validators.required],
-      more_sibling_genetic: ['', Validators.required],
-      more_sibling_history: ['', Validators.required],
-      more_sibling_medic: ['', Validators.required],
+      sibling2_medic_N_A: [false, Validators.required],
+      sibling2_medic_Blood: [false, Validators.required],
+      sibling2_medic_Heart: [false, Validators.required],
+      sibling2_medic_Respiratory: [false, Validators.required],
+      sibling2_medic_Gastrointestinal: [false, Validators.required],
+      sibling2_medic_Metabolic: [false, Validators.required],
+      sibling2_medic_Urinary: [false, Validators.required],
+      sibling2_medic_Reproductive: [false, Validators.required],
+      sibling2_medic_Neurological: [false, Validators.required],
+      sibling2_medic_Mental_Health: [false, Validators.required],
+      sibling2_medic_Muscle: [false, Validators.required],
+      sibling2_medic_Skin_Condition: [false, Validators.required],
+      sibling2_medic_Others: [false, Validators.required],
       explain: ['']
     });
+    this.siblingsFormGroup= this._formBuilder.group({
+      list_siblings: this._formBuilder.array([this.initListSiblings()],)
+    });
     this.secondFormGroup = this._formBuilder.group({
-      non_medical_drugs: [0, Validators.required],
-      tattoos: [0, Validators.required],
-      clotting: [0, Validators.required],
-      sexual_activity: [0, Validators.required],
-      sex_1_4: [0, Validators.required],
-      sex_12: [0, Validators.required],
-      HIV_infection: [0, Validators.required],
-      chronic: [0, Validators.required],
-      dwelling: [0, Validators.required],
-      exposed: [0, Validators.required],
-      suspected: [0, Validators.required],
-      received_blood: [0, Validators.required],
-      history_positive: [0, Validators.required],
-      cocaine: [0, Validators.required],
-      unsure: [0, Validators.required],
-      Alcoholism: [0, Validators.required],
-      juvenile: [0, Validators.required],
-      sex: [0, Validators.required],
-      Gonorrhea: [0, Validators.required],
-      Trichomonas: [0, Validators.required],
-      HSV: [0, Validators.required],
-      contaminate: [0, Validators.required],
-      deferred: [0, Validators.required],
-      Creutzfeldt: [0, Validators.required],
-      pituitary_derived: [0, Validators.required],
-      spent: [0, Validators.required],
-      spent_Northern_Europe: [0, Validators.required],
-      spent_more_years: [0, Validators.required],
-      past_120: [0, Validators.required],
-      close_contact_xenotransplantation: [0, Validators.required],
-      malaria: [0, Validators.required],
-      immunized: [0, Validators.required],
-      smallpox: [0, Validators.required],
-      diagnosed_following: [0, Validators.required],
-      family_diagnosed: [0, Validators.required],
-      exposed_radiation: [0, Validators.required],
-      suspected_sepsis: [0, Validators.required],
-      family_employed: [0, Validators.required],
-      evidence_infection: [0, Validators.required],
-      history_transmissible: [0, Validators.required],
-      tested_infectious: [0, Validators.required],
-      blood_transfusion: [0, Validators.required],
-      sexual_partner: [0, Validators.required],
-      excluded_blood_donation: [0, Validators.required],
-      relatives_medical_history: [0, Validators.required]
+      non_medical_drugs: ['', Validators.required],
+      tattoos: ['', Validators.required],
+      clotting: ['', Validators.required],
+      sexual_activity: ['', Validators.required],
+      sex_1_4: ['', Validators.required],
+      sex_12: ['', Validators.required],
+      HIV_infection: ['', Validators.required],
+      chronic: ['', Validators.required],
+      dwelling: ['', Validators.required],
+      exposed: ['', Validators.required],
+      suspected: ['', Validators.required],
+      received_blood: ['', Validators.required],
+      history_positive: ['', Validators.required],
+      cocaine: ['', Validators.required],
+      unsure: ['', Validators.required],
+      Alcoholism: ['', Validators.required],
+      juvenile: ['', Validators.required],
+      sex: ['', Validators.required],
+      Gonorrhea: ['', Validators.required],
+      Trichomonas: ['', Validators.required],
+      HSV: ['', Validators.required],
+      contaminate: ['', Validators.required],
+      deferred: ['', Validators.required],
+      Creutzfeldt: ['', Validators.required],
+      pituitary_derived: ['', Validators.required],
+      spent: ['', Validators.required],
+      spent_Northern_Europe: ['', Validators.required],
+      spent_more_years: ['', Validators.required],
+      past_120: ['', Validators.required],
+      close_contact_xenotransplantation: ['', Validators.required],
+      malaria: ['', Validators.required],
+      immunized: ['', Validators.required],
+      smallpox: ['', Validators.required],
+      diagnosed_following: ['', Validators.required],
+      family_diagnosed: ['', Validators.required],
+      exposed_radiation: ['', Validators.required],
+      suspected_sepsis: ['', Validators.required],
+      family_employed: ['', Validators.required],
+      evidence_infection: ['', Validators.required],
+      history_transmissible: ['', Validators.required],
+      tested_infectious: ['', Validators.required],
+      blood_transfusion: ['', Validators.required],
+      sexual_partner: ['', Validators.required],
+      excluded_blood_donation: ['', Validators.required],
+      relatives_medical_history: ['', Validators.required]
     });
     this.thirdFormGroup = this._formBuilder.group({
       reason_egg_donor: ['', Validators.required],
@@ -157,8 +267,8 @@ export class EggdonationComponent implements OnInit {
       history_abuse: ['', Validators.required]
     });
     this.fifthFormGroup = this._formBuilder.group({
-      imageUrl: [''],
-      videoUrl: ['']
+      imageUrl: ['', Validators.required],
+      videoUrl: ['', Validators.required]
     });
     this.sixthFormGroup = this._formBuilder.group({
       agree: ['', Validators.required],
@@ -166,8 +276,8 @@ export class EggdonationComponent implements OnInit {
     });
   }
 
-  imgSrc: String = '/assets/img/Upload_photos.png';
-  vidSrc: String = '/assets/img/Upload_video.png';
+  imgSrc: String = 'https://ibb.co/SVnhkRb';
+  vidSrc: String = 'https://ibb.co/X4927X0';
   urls = [];
   Vurls = [];
 
@@ -192,6 +302,49 @@ export class EggdonationComponent implements OnInit {
           this.Vurls.push(event.target.result);
         }
       }
+    }
+  }
+
+  initListSiblings() {
+    return this._formBuilder.group({
+      add: [''],
+      age: [''],
+      living: [''],
+      cause: [''],
+      ethnic: [''],
+      genetic: [''],
+      history: [''],
+      sibling_medic_N_A: [false],
+      sibling_medic_Blood: [false],
+      sibling_medic_Heart: [false],
+      sibling_medic_Respiratory: [false],
+      sibling_medic_Gastrointestinal: [false],
+      sibling_medic_Metabolic: [false],
+      sibling_medic_Urinary: [false],
+      sibling_medic_Reproductive: [false],
+      sibling_medic_Neurological: [false],
+      sibling_medic_Mental_Health: [false],
+      sibling_medic_Muscle: [false],
+      sibling_medic_Skin_Condition: [false],
+      sibling_medic_Others: [false]
+    });
+  }
+
+  addNewRow() {
+    const control = <FormArray>this.siblingsFormGroup.controls['list_siblings'];
+    control.push(this.initListSiblings());
+  }
+
+  deleteRow(index: number) {
+    const control = <FormArray>this.siblingsFormGroup.controls['list_siblings'];
+    if (control != null) {
+      this.TotalRow = control.value.length;
+    }
+    if (this.TotalRow > 1) {
+      control.removeAt(index);
+    } else {
+      alert('One record is mandatory.');
+      return false;
     }
   }
 
